1802807ff3269542578cdf61307a47f080f2478d
thewebrage <ein.nuff@gmail.com>
2020-12-01 20:40:04 -0700

    Removed changes

--- Models/User.cs
+++ Models/User.cs
@@ -11,5 +11,21 @@ namespace Assignment_2.Models
         public string UserName { get; set; }
         public string PasswordHash { get; set; }
         public string Salt { get; set; }
+        public string IsAdmin { get; set; }
+    }
+
+    public class TimeLog
+    {
+        public int ID { get; set; }
+        public User User { get; set; }
+        public DateTime StarTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Group
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
     }
 }
