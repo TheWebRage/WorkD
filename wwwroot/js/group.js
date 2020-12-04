b1f37049aeb6781c73bb1bc846f4a79ddb108bc5
Marc Taylor <taylorm096@gmail.com>
2020-12-03 19:44:02 -0700

    Added comments to group.js

--- wwwroot/js/group.js
+++ wwwroot/js/group.js
@@ -1,3 +1,6 @@
-﻿let data = { Bob: 27, Joe: 18, Frank: 14, Henry: 18, Paul: 70 };
+﻿
+////////    Format for using the pi chart with a JSON object    //////////////////////
+let data = { Bob: 27, Joe: 18, Frank: 14, Henry: 18, Paul: 70 };
 
+////////    Function to generate the pi chart
 generatePiChart(data);
