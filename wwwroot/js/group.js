f031a66b79b542f4e760c3501062fbc4b451f641
Marc Taylor <taylorm096@gmail.com>
2020-12-03 20:29:08 -0700

    Put dummy data in the combobox

--- wwwroot/js/group.js
+++ wwwroot/js/group.js
@@ -5,3 +5,22 @@ let data = { Bob: 27, Joe: 18, Frank: 14, Henry: 18, Paul: 70 };
 ////////    Function to generate the pi chart
 generatePiChart(data);
 
+function updateComboBox(data) {
+    let comboBox = d3.select('#combo')
+        .append('select');
+
+    for (let g of data) {
+        comboBox.append('option')
+            .attr('value', g)
+            .html(g);
+    }
+
+}
+
+let groups = ['group1', 'group2', 'group3'];
+
+updateComboBox(groups);
+
+function updateTable(data) {
+
+}
